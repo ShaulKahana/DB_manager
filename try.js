633b89dff81d095d81e4ea97026221d5478c0e27
Shaul Kahane <35196468+shaolkahna@users.noreply.github.com>
2022-12-20 10:51:10 +0200

    c

--- try.js
+++ try.js
@@ -1,50 +1,25 @@
-import { stat, createReadStream } from 'node:fs';
-import {open,appendFile} from 'node:fs/promises';
+
+const USER_BUF_LEN = 100;
+const FILL_CHAR = "~"
 
 
-async function get_user_data(userLineId){
+const buf = Buffer.alloc(USER_BUF_LEN, FILL_CHAR);
+const firstNamePlace = [0, 9];
+const lastNamePlace = [10, 19];
 
-    const file = await open("./db.txt");
+const firstName = "Yaki";
+const lastName = "Klein";
 
-    let curent_line_number = 1;
-    let me = {
-        encoding: null,
-        autoClose: true,
-        emitClose: true,
-        start: userLineId,
-        end : 59,
-        highWaterMark: 64 * 1024
-    }
-    
-    for await (const line of file.readLines(me)) {
-       
-      
-            console.log(line);
-    }
-
-    await file.close(file);
+for(let i = 0; i < firstName.length; i++) {
+    buf[firstNamePlace[0] + i] = firstName.charCodeAt(i)
 }
 
-get_user_data(0)
+for(let i = 0; i < lastName.length; i++) {
+    buf[lastNamePlace[0] + i] = lastName.charCodeAt(i)
+}
 
-// const readStream = createReadStream('./db.txt', {highWaterMark: 14});
-// const data = [];
+file.write(buf);
 
-// console.log(readStream);
 
-// readStream.on('data', (chunk) => {
-//     data.push(chunk);
-//     console.log('data :', chunk, chunk.length);
-//     // data : <Buffer 49 20 61 6d 20 74 72 61 6e 73 66 65 72 72 69 6e> 16
-//     // data : <Buffer 67 20 69 6e 20 62 79 74 65 73 20 62 79 20 62 79> 16
-//     // data : <Buffer 74 65 73 20 63 61 6c 6c 65 64 20 63 68 75 6e 6b> 16
-// });
+file.read(buf, 4 * USER_BUF_LEN)
 
-// readStream.on('end', () => {
-//     console.log(Buffer.concat(data).toString());
-//     // end : I am transferring in bytes by bytes called chunk
-// })
-
-// readStream.on('error', (err) => {
-//     console.log('error :', err)
-// })
